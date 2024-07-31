74e7a26ab79f1224904c03002ae4c076c9515480
Charles-DevFr <Charlesleul@gmail.com>
2024-07-31 14:12:13 +0200

    Presque fini

--- Projet The District test/TheDistrict_Commande.php
+++ Projet The District test/TheDistrict_Commande.php
@@ -1,119 +1,77 @@
-<?php
-    require_once('TheDistrict_header.php')
-    ?>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>The district Categorie</title>
+    <link rel="shortcut icon" type="image/png" href="image/the_district_brand/favicon.png">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+    <link rel="stylesheet" href="css/TheDistrict_Commande.css">
+</head>
+<body id="body">
     <div class="container">
-
-    
-
-    <div class="container" id="background-img"><br>
-<div class="row justify-content-center">
-
-<?php
-  $stmt=$dbh->prepare("SELECT plat.libelle AS nomplat, plat.image, plat.prix, plat.description, categorie.libelle AS nomcat, plat.id, id_categorie 
-                            FROM plat LEFT JOIN categorie ON plat.id_categorie=categorie.id 
-                                WHERE plat.id= :id ORDER BY categorie.libelle DESC");
-  try{
-    $stmt->execute(array(':id' => $_GET['comm'])); /* $stmt est un objet PDOStatement, qui représente une instruction SQL préparée.
-    execute() est une méthode de l'objet PDOStatement qui exécute l'instruction préparée.
-    Le array(':id' => $_GET['comm']) est un tableau associatif qui mappe un espace réservé :id dans l'instruction préparée à une valeur obtenue à partir du tableau superglobal $_GET. */
-  } catch (PDOException $e){
-    echo 'Erreur lors de l\'exécution de la requête : '. $e->getMessage();
-  }
-
-$result=$stmt->fetchAll(); /* $stmt est un objet PDOStatement, qui représente une instruction préparée qui a été exécutée.
-fetchAll() est une méthode de l'objet PDOStatement qui récupère toutes les lignes du jeu de résultats de l'instruction exécutée.
-Le résultat est stocké dans la variable $result, qui sera un tableau contenant toutes les lignes du jeu de résultats. */
-$stock=$_GET['comm']; /* $_GET : Il s'agit d'un tableau superglobal en PHP qui contient les variables transmises au script via la chaîne de requête URL. Il s'appelle $_GET car il récupère les données de l'URL à l'aide de la méthode HTTP GET.
-['comm'] : Il s'agit de la clé de la valeur récupérée du tableau $_GET. En d’autres termes, il s’agit du nom de la variable transmise dans la chaîne de requête URL. */
-?>
-
-<?php
-$i=0;
-foreach($result as $row){
-  echo '<span class="allplats col-3">
-          <div>
-            <div class="card mb-3 bg-secondary border border-2 border-dark" style="max-width: 600px; min-height: 300px;">
-              <div class="row g-0" id="cat">
-                <div class="col-md-4">
-                  <img src="image/food/'.$row['image'].'" class="img-fluid rounded-4 my-5 mx-2" alt="'.$row['nomplat'].'">
-                </div>
-                  <div class="col-md-8">
-                    <div class="card-body"><br>
-                      <h3 id="textcard" class="card-title">'.$row['nomplat'].'</h3><br>
-                      <h5 id="textcard" class="card-text">'.$row['description'].'</h5><br>
-                      <p class="card-text text-dark position-absolute bottom-0 start-0">Prix :<b> '.$row['prix'].' €</b></p>
-                        <div class="d-flex justify-content-end me-5">
-                          <button type="button" class="btn btn-danger d-flex position-absolute bottom-0 end-0">Commander</button><br>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </span>';
-  $i++;
-  if($i==1){
-    break;
-  }
-}
-/* '.$row['nomplat'].' est une variable PHP qui est concaténée (jointe) à la chaîne HTML environnante. Spécifiquement:
-$row est un tableau ou un objet contenant des données extraites d'une base de données ou d'une autre source de données.
-['nomplat'] accède à une clé ou une propriété spécifique dans le tableau/objet $row, qui contient la valeur du champ "nomplat".
-Le point (.) est l'opérateur de concaténation PHP, qui joint la chaîne HTML avec la valeur $row['nomplat']. */
-?>
-
-</div>
-
-<br><br>
-
-<div class="container">
-    <form id="formulaire" action="Script_commande.php" method="post">
-    <div class="row">
-  <div class="mb-3">
-    <label for="NometPrenom" class="form-label" id="NometPrenomCSS">Nom et prénom</label>
-    <input type="text" name="NometPrenom" class="form-control" id="NometPrenomJS">
-<p id="couleurtxt">Ce champ est obligatoire</p>
-  </div>
-  <div class="mb-3 col-6">
-    <label for="email" class="form-label" id="emailCSS">Email</label>
-    <input type="email" name="email" class="form-control" id="emailJS">
-<p id="couleurtxt">Ce champ est obligatoire</p>
-  </div>
-  <div class="mb-3 col-6">
-    <label for="telephone" class="form-label" id="telephoneCSS">Téléphone</label>
-    <input type="text" name="telephone" class="form-control" id="telephoneJS">
-<p id="couleurtxt">Ce champ est obligatoire</p>
-  </div>
-  <div class="mb-3">
-    <label for="votreadresse" class="form-label" id="votreadresseCSS">Votre adresse</label>
-    <textarea class="form-control" name="votreadresse" rows="3" id="votreadresseJS"></textarea>
-<p id="couleurtxt">Ce champ est obligatoire</p>    
-  </div>
-</div>
-</div>
-
-
-
-
-<div class="container">
-  <div class="row justify-content-end">
-  <button class="btn btn-secondary col-3" type="submit">Envoyer</button>
-</div>
-</div>
-</form>
-
-<br>
-
-</div>
-        
-    
-</div>
-
-    <?php
-    require_once('TheDistrict_footer.php')
-?>
-
-
-<script src="js/TheDistrict_Commande.js"></script>
+        <?php
+        require_once("TheDistrict_header.php");
+        ?>
+          <br>
+          <section class="imagefond">
+          <br><br>
+      <div class="container">
+        <form id="form" action="script/script_commande.php" method="post">
+        <div class="row d-flex justify-content-center" id="cardglobal">
+          <div class="card mb-3" id="cardcommand" style="max-width: 540px;">
+            <div class="row g-0">
+            <div class="col-md-4">
+            <img src="..." class="img-fluid rounded-start" alt="">
+            </div>
+            <div class="col-md-8">
+            <div class="card-body">
+              <h5 id="titrecard" class="card-title">Plat commandé</h5>
+              <p id="descriptioncard" class="card-text">La description du plat commandé</p>
+              <p id="quantitecard" class="card-text">Quantité</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    </div>
+    <br><br>
+    <div class="container">
+      <div class="row">    
+       <div class="col-12">
+        <label for="inputname" class="form-label" id="textform">Nom prénom</label>
+        <input type="text" name="nom+prenom class="form-control" id="nom+prenom">
+        <p>Ce champ est obligatoire</p>
+      </div>
+      <div class="col-6">
+        <label for="inputemail" class="form-label" id="textform">Email address</label>
+        <input type="email" name="email" class="form-control" id="email"laceholder="Charles-DevFr@Gmail.com">
+        <p>Ce champ est obligatoire</p>
+      </div>
+      <div class="col-6">
+        <label for="inputphone" class="form-label" id="textform">Téléphone</label>
+        <input type="number" name="telephone" class="form-control" id="telephone">
+        <p>Ce champ est obligatoire</p>
+      </div>
+      <div>
+        <label for="inputcity" class="form-label" id="textform">Votre adresse</label>
+        <textarea class="form-control" name="adresse" id="adresse" rows="8"></textarea>
+        <p>Ce champ est obligatoire</p>
+      </div>
+     </div>
+    </div>
+    <div class="container">
+      <button type="submit" class="btn btn-secondary btn-lg" id="bouton" >Envoyer</button>
+    </div>
+     </section>
+      <br><br>
+      <div class="container">
+        <?php
+        require_once("TheDistrict_footer.php");
+        ?>
+      </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    <script src="js/TheDistrict_Commande.js"></script>
 </body>
 </html>
--- Projet The District test/TheDistrict_Contact.php
+++ Projet The District test/TheDistrict_Contact.php
@@ -1,66 +1,70 @@
-<?php
-    require_once('TheDistrict_header.php')
-    ?>
-  
-  <div class="container">
-
-        <section class="container" id="background-img"><br>
-    
-    
-    <br><br><br><br><br><br>
-    
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>The district Contact</title>
+    <link rel="shortcut icon" type="image/png" href="image/the_district_brand/favicon.png">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+    <link rel="stylesheet" href="css/TheDistrict_Contact.css">
+</head>
+<body id="body">
     <div class="container">
-        <form id="formulaire" action="script/script_contact.php" method="post">
-        <div class="row">
-      <div class="mb-3 col-6">
-        <label for="NometPrenom" class="form-label" id="NometPrenomCSS">Nom</label>
-        <input type="text" name="nom" class="form-control" id="NomJS">
-    <p id="couleurtxt">Ce champ est obligatoire</p>
-      </div>
-      <div class="mb-3 col-6">
-        <label for="NometPrenom" class="form-label" id="NometPrenomCSS">Prénom</label>
-        <input type="text" name="prenom" class="form-control" id="PrenomJS">
-      </div>
-      <div class="mb-3 col-6">
-        <label for="email" class="form-label" id="emailCSS">Email</label>
-        <input type="email" name="email" class="form-control" id="emailJS">
-    <p id="couleurtxt">Ce champ est obligatoire</p>
-      </div>
-      <div class="mb-3 col-6">
-        <label for="telephone" class="form-label" id="telephoneCSS">Téléphone</label>
-        <input type="text" name="telephone" class="form-control" id="telephoneJS">
-    <p id="couleurtxt">Ce champ est obligatoire</p>
-      </div>
-      <div class="mb-3">
-        <label for="votreadresse" class="form-label" id="votreadresseCSS">Votre demande</label>
-        <textarea class="form-control" name="votreadresse" rows="3" id="votreadresseJS"></textarea>
-    <p id="couleurtxt">Ce champ est obligatoire</p>    
-      </div>
-    </div>
-    </div>
-    
-    
-    
-    
-    <div class="container">
-      <div class="row justify-content-end">
-      <button type="submit" class="btn btn-primary col-3">Envoyer</button>
-    </div>
-    </div>
-    </form>
-    
-    <br>
-    
-    </section>
-            
-        <br>
-    
         <?php
-    require_once('TheDistrict_footer.php')
-?>
-
+        require_once("TheDistrict_header.php");
+        ?>
+      <section class="row">
+        <div>
+          <svg  class="d-none d-md-block animation h-25 position-absolute bottom-50">
+                <text x="50%" y="50%" dy= "68px" text-anchor="middle">
+                    The District
+                </text>
+            </svg>
         </div>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
-    <script src="js/TheDistrict_Contact.js"></script> 
-  </body>
-    </html>
+    </section>
+      <br><br>
+      <div class="container">
+      <form id="form" action="script/script_contact.php" method="post">
+        <form id="formulaire">
+        <div class="row">
+          <div class="mb-4 col-6">
+            <label for="InputName" class="form-label">Nom</label>
+            <input type="text" name="nom" class="form-control" id="nom">
+            <p>Ce champ est obligatoire</p>
+          </div>
+          <div class="mb-4 col-6">
+            <label for="InputName" class="form-label">Prénom</label>
+            <input type="text" name="prenom" class="form-control" id="prenom">
+          </div>
+          <div class="mb-4 col-6">
+            <label for="email" class="form-label">Email address</label>
+            <input type="text" name="email" class="form-control" id="email" placeholder="Charles-DevFr@Gmail.com">
+          </div>
+          <div class="mb-4 col-6">
+            <label for="phone" class="form-label">Téléphone</label>
+            <input type="text" name="telephone" class="form-control" id="telephone">
+            <p>Ce champ est obligatoire</p>
+          </div>
+          <div class="mb-4 col-12">
+            <label for="exampleFormControlTextarea1" class="form-label">Votre demande</label>
+            <textarea class="form-control" name="adresse" id="demande" rows="3"></textarea>
+          </div>
+        </div>
+      </div>
+      <div class="container">
+        <button type="submit" class="btn btn-secondary btn-lg" id="bouton" >Envoyer</button>
+      </div>
+    </form>
+  </div>
+      <br><br>
+      <div class="container">
+        <?php
+        require_once("TheDistrict_footer.php");
+        ?>
+      </div>
+    </div>
+    <script src="js/TheDistrict_Contact.js"></script>
+      <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+</body>
+</html>
